2c02f30c83ae78c8c18a71f5e00164a471394181
Andrés Ortega <arguello.25018@gmail.com>
2023-08-24 14:37:29 -0400

    cambios vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: 'https://amortega.github.io/videos-app-frontend/'
 })
